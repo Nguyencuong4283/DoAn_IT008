86dbf1af549d17e967ccc0239f96bffd37ac8a5a
LeVanAn <levanan1902006@nan1902006gmail.06gmailcom>
2025-09-23 22:54:42 +0700

    Thêm Binding từ user lên userviewmodel.Có thể thêm sảm phẩm ở dạng new.

--- Model/SanPham.cs
+++ Model/SanPham.cs
@@ -1,30 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShopLink.Model
 {
     public class SanPham
     {
         public string MaSanPham { get; set; }
-        public string NameSanPham { get; set; }
-        public string MoTaSanPham {  set; get; }
-        public DateOnly NgayHetHan {  get; set; }
+        public string TenSanPham { get; set; }
+        public string MoTaSanPham { get; set; }
+        public DateOnly NgayHetHan { get; set; }
         public int SoLuong { get; set; }
-        public double Gia {  get; set; }
 
+        public double GiaGoc { get; set; }
+        public double KhuyenMai { get; set; }
+
+        // Giá bán tính tự động từ Giá gốc và Khuyến mãi
+        public double GiaBan => GiaGoc - GiaGoc * KhuyenMai / 100;
+
+        public string HinhAnh { get; set; }
         public string MaStore { get; set; }
 
         public SanPham() { }
-        public SanPham(string ten, string mota, DateOnly dateOnly, int sl, int gia)
+
+        public SanPham(string t, double g, double km, string ha)
         {
-            NameSanPham = ten;
+            TenSanPham = t;
+            GiaGoc = g;
+            KhuyenMai = km;
+            MoTaSanPham = ha;
+        }
+
+        public SanPham(string ten, string mota, DateOnly dateOnly, int sl, double gia, double khuyenMai, string hinhAnh)
+        {
+            TenSanPham = ten;
             MoTaSanPham = mota;
             NgayHetHan = dateOnly;
             SoLuong = sl;
-            Gia = gia;
+            GiaGoc = gia;
+            KhuyenMai = khuyenMai;
+            HinhAnh = hinhAnh;
         }
     }
 }
--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using ShopLink.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopLink.ViewModel
 {
-    class UserViewModel
+    public class UserViewModel: INotifyPropertyChanged
     {
+        private ObservableCollection<SanPham> _sanPhams;
+        public ObservableCollection<SanPham> SanPhams
+        {
+            get => _sanPhams;
+            set
+            {
+                _sanPhams = value;
+                OnPropertyChanged(nameof(SanPhams));
+            }
+        }
+
+        public UserViewModel()
+        {
+            // Khởi tạo danh sách sản phẩm mẫu
+            SanPhams = new ObservableCollection<SanPham>
+        {
+            new SanPham
+            {
+                TenSanPham = "Sản phẩm 1",
+                GiaGoc = 120000,
+                KhuyenMai = 10,
+                HinhAnh = "C:\\Users\\Admin\\source\\repos\\ShopLink\\Asset\\Images\\Demo.jpg"
+            } ,
+            new SanPham
+            {
+                TenSanPham = "Sản phẩm 2",
+                GiaGoc = 250000,
+                KhuyenMai = 20,
+                HinhAnh = "C:\\Users\\Admin\\source\\repos\\ShopLink\\Asset\\Images\\Demo.jpg"
+            }
+        };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
